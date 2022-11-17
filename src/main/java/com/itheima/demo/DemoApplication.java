b76d7a30bd75e0007e0af984839f8daf6c3b2e85
lihaiming <lihaiming@csii.com>
2022-11-17 22:00:56 +0800

    hot-fix commit2

--- src/main/java/com/itheima/demo/DemoApplication.java
+++ src/main/java/com/itheima/demo/DemoApplication.java
@@ -16,6 +16,8 @@ public class DemoApplication {
         System.out.println("2");
         System.out.println("3");
         System.out.println("4");
+        System.out.println("hot-fix test");
+
     }
 
 }
