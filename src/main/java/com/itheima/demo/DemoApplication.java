aca0b7f2d753762c1ce124c22dac2b392930384e
lihaiming <lihaiming@csii.com>
2022-11-17 21:55:54 +0800

    hot-fix first

--- src/main/java/com/itheima/demo/DemoApplication.java
+++ src/main/java/com/itheima/demo/DemoApplication.java
@@ -15,6 +15,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("2");
         System.out.println("3");
+        System.out.println("4");
     }
 
 }
